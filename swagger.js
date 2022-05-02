97496ba2c78e0293320a1a28d5420e22121e4404
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-02 11:25:17 -0600

    add swagger.js autogen file

---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Lindsey Myers - API',
+    description: 'CSE 341 Project 2',
+  },
+  host: 'cse341lmyersproject2.herokuapp.com',
+  schemes: ['https'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./backend/routes/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
