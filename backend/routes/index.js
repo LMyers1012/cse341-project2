ea8beef0238a9d1b0f5554354eac6ddbc5cfd9ce
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-06 13:54:26 -0600

    fix file path user

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 router.use('/students', require('./student'));
 router.use('/instructors', require('./instructor'));
-router.use('/users', require('./user.js'));
+router.use('/user', require('./user.js'));
 
 module.exports = router;
