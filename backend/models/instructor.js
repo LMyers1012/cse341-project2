b1be9145a2cfb63784e37412a34a09f45f9876e7
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-06 14:01:09 -0600

    add joi validation

--- backend/models/instructor.js
+++ backend/models/instructor.js
@@ -16,16 +16,17 @@
 // };
 
 // const array = require(joi / lib / types / array);
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const InstructorSchema = mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  birthday: { type: String },
-  beltLevel: { type: String },
-  style: { type: [String] },
-  strengths: { type: [String] },
-  classes: { type: [String] },
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  birthday: Joi.string().required(),
+  beltLevel: Joi.string().required(),
+  style: Joi.string().required(),
+  strengths: Joi.string().required(),
+  classes: Joi.string().required(),
 });
 
 module.exports = mongoose.model('instructors', InstructorSchema);
