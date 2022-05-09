8fa2ffad01e07d1a5c1e0bb31b6d4cfd5f27cbd3
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-09 08:40:13 -0600

    fix server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./backend/db/connect');
+// const mongodb = require('./backend/db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -9,32 +9,30 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    // res.setHeader('Content-Type', 'application/json');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS'
-    );
     next();
   })
   .use('/', require('./backend/routes'));
 
 // Catch-all for logging errors in console
-process.on('uncaughtException', (err, origin) => {
-  console.log(
-    process.stderr.fd,
-    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
-  );
-});
+// process.on('uncaughtException', (err, origin) => {
+//   console.log(
+//     process.stderr.fd,
+//     `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+//   );
+// });
 
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on port ${port}`);
-  }
-});
+const db = require('./backend/models');
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Database connected and server running on port ${port}.`);
+    });
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database.', err);
+    process.exit();
+  });
