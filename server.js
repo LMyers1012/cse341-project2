f9bfb79a703917a466fa76fb7c455f0cb89e629f
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-11 12:23:44 -0600

    reorder server.js

--- server.js
+++ server.js
@@ -12,14 +12,30 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Passport Config
+require('./backend/db/passport')(passport);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Database connected and server running on port ${port}.`);
+    });
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database.', err);
+    process.exit();
+  });
+
 // Logging while in dev mode
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Passport Config
-require('./backend/db/passport')(passport);
-
+// Handlebars
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
@@ -40,6 +56,7 @@ app.use(passport.session());
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
@@ -66,18 +83,3 @@ process.on('uncaughtException', (err, origin) => {
     `Caught exception: ${err}\n` + `Exception origin: ${origin}`
   );
 });
-
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Database connected and server running on port ${port}.`);
-    });
-  })
-  .catch((err) => {
-    console.log('Cannot connect to the database.', err);
-    process.exit();
-  });
