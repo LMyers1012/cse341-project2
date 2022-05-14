7bdb4c549e36107d457dca4f10743edfc86de9ab
Lindsey Myers <candlmyers1012@gmail.com>
2022-05-14 12:18:19 -0600

    update callback uri in passport.js

--- backend/db/passport.js
+++ backend/db/passport.js
@@ -10,7 +10,7 @@ module.exports = function (passport) {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          'https://cse341lmyersproject2.herokuapp.com/auth/google/callback',
+          'https://cse341lmyersproject2.herokuapp.com/auth/google/callback/',
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
